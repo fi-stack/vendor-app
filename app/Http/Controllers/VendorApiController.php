cb194b06877c2fc565f66be8b6ea1f20f4f75e2b
rafi-windows <rafi_n_o@github.com>
2021-02-18 10:37:18 +0700

    create rest api vendor

--- app/Http/Controllers/VendorApiController.php
+++ app/Http/Controllers/VendorApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Vendor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class VendorApiController extends Controller
 {
@@ -27,5 +28,42 @@ class VendorApiController extends Controller
             'message' => 'Detail Data Vendor',
             'data' => $post
         ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nama_vendor' => 'required',
+            'alamat' => 'required',
+            'kota' => 'required',
+            'provinsi' => 'required',
+            'kode_pos' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $post = Vendor::create([
+            'nama_vendor' => $request->nama_vendor,
+            'alamat' => $request->alamat,
+            'kota' => $request->kota,
+            'provinsi' => $request->provinsi,
+            'kode_pos' => $request->kode_pos,
+        ]);
+
+        // success save to database
+        if ($post) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Vendor Created',
+                'data' => $post
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Vendor Failed to Save',
+        ], 409);
     }
 }
