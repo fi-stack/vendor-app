dd64c4ce38e712e2d12c1ddb63a440c2ce0c36e4
rafi-windows <rafi_n_o@github.com>
2021-02-18 10:17:27 +0700

    message

--- resources/views/vendor/index.blade.php
+++ resources/views/vendor/index.blade.php
@@ -1,67 +1,94 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Document</title>
+        <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        />
+        <link
+            rel="stylesheet"
+            href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
+        />
+    </head>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-</head>
-
-<body style="background: lightgray">
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card border-0 shadow rounded">
-                    <div class="card-body">
-                        <a href="{{ route('vendor.create') }}" class="btn btn-md btn-success mb-3">Tambah Vendor</a>
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Nama Vendor</th>
-                                    <th scope="col">Alamat</th>
-                                    <th scope="col">Kota</th>
-                                    <th scope="col">Provinsi</th>
-                                    <th scope="col">Kode Pos</th>
-                                    <th scope="col">Opsi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @forelse ($vendors as $vendor)
-                                <tr>
-                                    <td>{{ $vendor->nama_vendor }}</td>
-                                    <td>{{ $vendor->alamat }}</td>
-                                    <td>{{ $vendor->kota }}</td>
-                                    <td>{{ $vendor->provinsi }}</td>
-                                    <td>{{ $vendor->kode_pos }}</td>
-                                    <td class="text-center">
-                                        <form onsubmit="return confirm('Apakah anda yakin?')" action="{{ route('vendor.destroy', $vendor->id) }}" method="POST">
-                                            <a href="{{ route('vendor.edit', $vendor->id) }}" class="btn btn-sm btn-primary">Edit</a>
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-sm btn-danger">Hapus</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                                @empty
-                                <div class="alert alert-danger">
-                                    Data vendor belum tersedia.
-                                </div>
-                                @endforelse
-                            </tbody>
-                        </table>
-                        {{ $vendors->links() }}
+    <body style="background: lightgray">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card border-0 shadow rounded">
+                        <div class="card-body">
+                            <a
+                                href="{{ route('vendor.create') }}"
+                                class="btn btn-md btn-success mb-3"
+                                >Tambah Vendor</a
+                            >
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Nama Vendor</th>
+                                        <th scope="col">Alamat</th>
+                                        <th scope="col">Kota</th>
+                                        <th scope="col">Provinsi</th>
+                                        <th scope="col">Kode Pos</th>
+                                        <th scope="col">Opsi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($vendors as $vendor)
+                                    <tr>
+                                        <td>{{ $vendor->nama_vendor }}</td>
+                                        <td>{{ $vendor->alamat }}</td>
+                                        <td>{{ $vendor->kota }}</td>
+                                        <td>{{ $vendor->provinsi }}</td>
+                                        <td>{{ $vendor->kode_pos }}</td>
+                                        <td class="text-center">
+                                            <form
+                                                onsubmit="return confirm('Apakah anda yakin?')"
+                                                action="{{ route('vendor.destroy', $vendor->id) }}"
+                                                method="POST"
+                                            >
+                                                <a
+                                                    href="{{ route('vendor.edit', $vendor->id) }}"
+                                                    class="btn btn-sm btn-primary"
+                                                    >Edit</a
+                                                >
+                                                @csrf @method('DELETE')
+                                                <button
+                                                    type="submit"
+                                                    class="btn btn-sm btn-danger"
+                                                >
+                                                    Hapus
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    @empty
+                                    <div class="alert alert-danger">
+                                        Data vendor belum tersedia.
+                                    </div>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                            {{ $vendors->links() }}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
-</body>
-
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+        <script>
+            // message with tastr
+            @if(session()->has('success'))
+            toastr.success('{{ session('success') }}', 'BERHASIL!');
+            @elseif(session()->has('error'))
+            toastr.error('{{ session('error') }}', 'GAGAL!');
+            @endif
+        </script>
+    </body>
 </html>
