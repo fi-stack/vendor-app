4afb97d261883b6ea1aca55f357eae83427cc582
rafi-windows <rafi_n_o@github.com>
2021-02-18 10:08:51 +0700

    create input vendor

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -44,4 +44,53 @@ class VendorController extends Controller
             return redirect()->route('vendor.index')->with(['error' => 'Data gagal disimpan!']);
         }
     }
+
+    public function edit(Vendor $vendor)
+    {
+        return view('vendor.edit', compact('vendor'));
+    }
+
+    public function update(Request $request, Vendor $vendor)
+    {
+        $this->validate($request, [
+            'nama_vendor' => 'required',
+            'alamat' => 'required',
+            'kota' => 'required',
+            'provinsi' => 'required',
+            'kode_pos' => 'required',
+        ]);
+
+        // get data vendor by Id
+        $vendor = vendor::findOrFail($vendor->id);
+
+        $vendor->update([
+            'nama_vendor' => $request->nama_vendor,
+            'alamat' => $request->alamat,
+            'kota' => $request->kota,
+            'provinsi' => $request->provinsi,
+            'kode_pos' => $request->kode_pos,
+        ]);
+
+        if ($vendor) {
+            // redirect dengan pesan sukses
+            return redirect()->route('vendor.index')->with(['success' => 'Data berhasil diupdate!']);
+        } else {
+            // redirect dengan pesan error
+            return redirect()->route('vendor.index')->with(['error' => 'Data Gagal diupdate!']);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $vendor = Vendor::findOrFail($id);
+        $vendor->delete();
+
+        if ($vendor) {
+            // redirect dengan pesan sukses
+            return redirect()->route('vendor.index')->with(['success' => 'Data berhasil dihapus!']);
+        } else {
+            // redirect dengan pesan error
+            return redirect()->route('vendor.index')->with(['error' => 'Data gagal dihapus!']);
+        }
+    }
 }
